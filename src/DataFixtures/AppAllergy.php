2833a0d7542925b13f210ef067c583d8e3b2d59f
aline_pencreach <aline.pencreach@gmail.com>
2023-01-24 15:18:52 +0100

    test fixtures allergy ok

--- src/DataFixtures/AppAllergy.php
+++ src/DataFixtures/AppAllergy.php
@@ -11,6 +11,8 @@ class AppAllergy extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker\Factory::create('fr_FR');
+        
         for ($i=0; $i < 10; $i++) { 
             $allergy = new Allergy();
             $allergy->setName($faker->lexify('????????'));
